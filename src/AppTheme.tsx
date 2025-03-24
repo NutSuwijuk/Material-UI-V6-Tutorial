2b9442d6e07dcc4a4bc57877789ae252d052934e
Suwijuk <suwijuk2261@gmail.com>
2025-03-24 11:59:12 +0700

    Theme Button (outlined)

--- src/AppTheme.tsx
+++ src/AppTheme.tsx
@@ -2,12 +2,24 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import React from "react";
 // Supports weights 100-900
 import "@fontsource-variable/inter";
-import { Button } from "@mui/material";
+// import { Button } from "@mui/material";
 
 const theme = createTheme({
   palette: {
     primary: {
       main: "#111827",
+    },
+    grey: {
+      "50": "#fafafa",
+      "100": "#f5f5f5",
+      "200": "#e5e5e5",
+      "300": "#d4d4d4",
+      "400": "#a3a3a3",
+      "500": "#737373",
+      "600": "#525252",
+      "700": "#404040",
+      "800": "#262626",
+      "900": "#171717",
     },
     background: {
       default: "#f5f5f5",
@@ -38,6 +50,15 @@ const theme = createTheme({
                 },
                 "&:active": {
                   boxShadow: "none",
+                },
+              },
+            },
+            {
+              props: { variant: "outlined", color: "primary" },
+              style: {
+                borderColor: theme.palette.grey[300],
+                "&:hover": {
+                  borderColor: theme.palette.grey[400],
                 },
               },
             },
